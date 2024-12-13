a08be587bc04d8b18c055bff57634db9688c7ff8
Stiven1999A <eandrey.gonzalez@udea.edu.co>
2024-12-13 09:50:04 -0500

    updated novelty_detector

--- app/scripts/novelty_detection.py
+++ app/scripts/novelty_detection.py
@@ -4,14 +4,45 @@ import numpy as np
 import scipy.stats as stats
 
 def segment_data(df):
-    #df['Fecha'] = pd.to_datetime(df['Fecha'])
-    segment1 = df[df['Fecha'] <= '2021-01-31']
-    segment2 = df[(df['Fecha'] > '2021-01-31') & (df['Fecha'] <= '2021-02-28')]
-    segment3 = df[(df['Fecha'] > '2021-02-28') & (df['Fecha'] <= '2021-03-20')]
-    segment4 = df[(df['Fecha'] > '2021-03-20') & (df['Fecha'] <= '2021-03-31')]
-    return [segment1, segment2, segment3, segment4]
+    """
+    Segments the input DataFrame into predefined date ranges.
+    Args:
+        df (pd.DataFrame): Input DataFrame containing a 'Fecha' column with date values.
+    Returns:
+        list of pd.DataFrame: A list of DataFrames, each corresponding to a segment of the input DataFrame
+                              within the specified date ranges.
+    """
+
+    df['Fecha'] = pd.to_datetime(df['Fecha'])
+    segments = []
+    date_ranges = [
+        ('2021-01-01', '2021-01-31'),
+        ('2021-02-01', '2021-02-28'),
+        ('2021-03-01', '2021-03-20'),
+        ('2021-03-21', '2021-03-31')
+    ]
+    
+    for start_date, end_date in date_ranges:
+        segment = df[(df['Fecha'] >= start_date) & (df['Fecha'] <= end_date)]
+        segments.append(segment)
+    
+    return segments
 
 def label_atypical_values(new_consumptions, method='MAD', stored_consumptions=None):
+    """
+    Labels atypical values based on the 'ConsumoMIPS' column of the new_consumptions DataFrame.
+    Parameters:
+    new_consumptions (pd.DataFrame): DataFrame containing the new consumption data with a 'ConsumoMIPS' column.
+    method (str, optional): Method to use for detecting atypical values. Options are 'MAD' (Median Absolute Deviation) 
+                            and 'IQR' (Interquartile Range). Default is 'MAD'.
+    stored_consumptions (array-like, optional): Array-like object containing stored consumption values to use for 
+                                                calculating the median and MAD or IQR. If None, calculations are based 
+                                                on new_consumptions. Default is None.
+    Returns:
+    pd.DataFrame: The input DataFrame with an additional column 'IdAtipico' where -1 indicates a low atypical value, 
+                  1 indicates a high atypical value, and 0 indicates a typical value.
+    """
+    
     if method == 'MAD':
         if stored_consumptions is None:
             m = new_consumptions['ConsumoMIPS'].median()
@@ -52,7 +83,21 @@ def label_atypical_values(new_consumptions, method='MAD', stored_consumptions=No
     return new_consumptions
 
 def detect_atypical_values(conn_insert, df: pd.DataFrame):
-
+    """Detects atypical values in the given DataFrame and inserts the processed data into the database.
+    Parameters:
+    conn_insert (pyodbc.Connection): The database connection object used for inserting data.
+    df (pd.DataFrame): The input DataFrame containing the data to be processed.
+    Returns:
+    str: A message indicating the result of the operation.
+    Notes:
+    - The function renames specific columns in the DataFrame for consistency.
+    - It assigns unique IDs to each row in the DataFrame.
+    - If the DataFrame is empty, it returns the DataFrame as is.
+    - The function processes the data in segments and labels atypical values using different methods (MAD, IQR) based on the data characteristics.
+    - The processed data is inserted into the database in batches to optimize performance.
+    - The function handles both initial data insertion and updates to existing data.
+    - It prints progress messages to indicate the status of the operation.
+    """
     if df.empty:
         return df
 
@@ -68,13 +113,35 @@ def detect_atypical_values(conn_insert, df: pd.DataFrame):
 
     df_to_insert = pd.DataFrame()
 
+    def insert_data(df_to_insert):
+        insert_query = """
+            INSERT INTO dbo.ConsumosMIPS (IdConsumo, IdProceso, IdGrupo, IdFecha, IdDiaSemana, IdAtipico, Ejecuciones, ConsumoMIPS)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """
+        data_to_insert = [
+            (
+                int(row['IdConsumo']),
+                int(row['IdProceso']),
+                int(row['IdGrupo']),
+                int(row['IdFecha']),
+                int(row['IdDiaSemana']),
+                int(row['IdAtipico']),
+                float(row['Ejecuciones']),
+                float(row['ConsumoMIPS'])
+            )
+            for _, row in df_to_insert.iterrows()
+        ]
+        
+        start_time = time.time()
+        for i in range(0, len(data_to_insert), 1000):
+            cursor.executemany(insert_query, data_to_insert[i:i+1000])
+            conn_insert.commit()
+            if time.time() - start_time > 600:
+                print("Process is still running...")
+                start_time = time.time()
+
     if last_id == 0:
-
-        df = df[
-            ['IdConsumo', 'IdProceso', 'IdGrupo', 'IdFecha', 'IdDiaSemana',
-            'IdAtipico', 'Ejecuciones', 'ConsumoMIPS', 'Fecha']
-            ]
-
+        df = df[['IdConsumo', 'IdProceso', 'IdGrupo', 'IdFecha', 'IdDiaSemana', 'IdAtipico', 'Ejecuciones', 'ConsumoMIPS', 'Fecha']]
         count_df = df['IdProceso'].value_counts().reset_index()
         count_df.columns = ['IdProceso', 'Count']
         df_idprocess_one_execution = count_df[count_df['Count'] == 1]
@@ -105,39 +172,12 @@ def detect_atypical_values(conn_insert, df: pd.DataFrame):
                             daily_segment = label_atypical_values(daily_segment, method='MAD')
 
                     df_to_insert = pd.concat([df_to_insert, daily_segment], ignore_index=True)
-        print("Updating the ConsumosMIPS table.")
-        insert_query = """
-            INSERT INTO dbo.ConsumosMIPS (IdConsumo, IdProceso, IdGrupo, IdFecha, IdDiaSemana, IdAtipico, Ejecuciones, ConsumoMIPS)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-        """
-        data_to_insert = [
-            (
-                int(row['IdConsumo']),
-                int(row['IdProceso']),
-                int(row['IdGrupo']),
-                int(row['IdFecha']),
-                int(row['IdDiaSemana']),
-                int(row['IdAtipico']),
-                float(row['Ejecuciones']),
-                float(row['ConsumoMIPS'])
-            )
-            for _, row in df_to_insert.iterrows()
-        ]
         
-        start_time = time.time()
-        for i in range(0, len(data_to_insert), 1000):
-            cursor.executemany(insert_query, data_to_insert[i:i+1000])
-            conn_insert.commit()
-            if time.time() - start_time > 600:
-                print("Process is still running...")
-                start_time = time.time()
+        print("Updating the ConsumosMIPS table.")
+        insert_data(df_to_insert)
 
     else:
-
-        df = df[
-            ['IdConsumo', 'IdProceso', 'IdGrupo', 'IdFecha', 'IdDiaSemana',
-            'IdAtipico', 'Ejecuciones', 'ConsumoMIPS']
-            ]
+        df = df[['IdConsumo', 'IdProceso', 'IdGrupo', 'IdFecha', 'IdDiaSemana', 'IdAtipico', 'Ejecuciones', 'ConsumoMIPS']]
         
         cursor.execute("SELECT IdFecha FROM dbo.Fechas WHERE Fecha = '2021-03-20'")
         start_id_fecha = cursor.fetchone()[0]
@@ -161,7 +201,6 @@ def detect_atypical_values(conn_insert, df: pd.DataFrame):
         
                 if len(stored_consumptions) == 0:
                     new_consumption['IdAtipico'] = 1
-                
                 elif len(stored_consumptions) == 1:
                     if (new_consumption['ConsumoMIPS'].values[0] - stored_consumptions[0]) > 1:
                         new_consumption['IdAtipico'] = 1
@@ -181,32 +220,7 @@ def detect_atypical_values(conn_insert, df: pd.DataFrame):
                 df_to_insert = pd.concat([df_to_insert, new_consumption], ignore_index=True)
             
             print("Updating the ConsumosMIPS table.")
-            insert_query = """
-                INSERT INTO dbo.ConsumosMIPS (IdConsumo, IdProceso, IdGrupo, IdFecha, IdDiaSemana, IdAtipico, Ejecuciones, ConsumoMIPS)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            """
-            data_to_insert = [
-                (
-                    int(row['IdConsumo']),
-                    int(row['IdProceso']),
-                    int(row['IdGrupo']),
-                    int(row['IdFecha']),
-                    int(row['IdDiaSemana']),
-                    int(row['IdAtipico']),
-                    float(row['Ejecuciones']),
-                    float(row['ConsumoMIPS'])
-                )
-                for _, row in df_to_insert.iterrows()
-            ]
-            
-            start_time = time.time()
-            for i in range(0, len(data_to_insert), 1000):
-                cursor.executemany(insert_query, data_to_insert[i:i+1000])
-                conn_insert.commit()
-                if time.time() - start_time > 600:
-                    print("Process is still running...")
-                    start_time = time.time()
-
+            insert_data(df_to_insert)
             df_to_insert = pd.DataFrame()
 
     return 'Data updated successfully'
